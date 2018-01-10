8d7d77f72501a76589fb4dd1c0cd7bb00960f2b0
Johan Buts <johan.buts@trustedfamily.net>
2018-01-10 14:47:23 +0100

    Cleanup of unused code

--- src/utils/datesInVisualMonth.js
+++ src/utils/datesInVisualMonth.js
@@ -1,26 +1,3 @@
-// export default function datesInMonthFrom(firstDayOfWeek, year, month) {
-//   var rows = [],
-//     dates = [],
-//     i = 0,
-//     date = new Date(year, month, 1);
-
-//   while (date.getDay() !== (firstDayOfWeek || 0)) {
-//     date.setDate(date.getDate() - 1);
-//   }
-
-//   do {
-//     for (i = 0; i < 7; i++) {
-//       dates.push(date);
-//       date = new Date(date.getTime());
-//       date.setDate(date.getDate() + 1);
-//     }
-//     rows.push(dates);
-//     dates = [];
-//   } while ((date.getMonth() <= month) && (date.getFullYear() === year));
-
-//   return rows;
-// }
-
 const DAYS_IN_WEEK = 7
 const DAY = 24 * 60 * 60 * 1000
 
